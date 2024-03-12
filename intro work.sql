eb19ef17ae24548be3be001cb7ba2719758d84f6
bosedeosayande <133767029+bosedeosayande@users.noreply.github.com>
2024-03-12 21:20:16 +0100

    Add files via upload

---
+++ intro work.sql
@@ -0,0 +1,8 @@
+SELECT TOP (1000) [EmployerID]
+      ,[FirstName]
+      ,[LastName]
+      ,[Age]
+      ,[Gernder]
+  FROM [SQL Project 1].[dbo].[EmployeeDemographics]
+
+  create view employeeDemographics
