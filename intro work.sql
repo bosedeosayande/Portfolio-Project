ca27bfe7a95a8c4689d3d2206c5c25b000b8a749
Bosede Osayande <133767029+bosedeosayande@users.noreply.github.com>
2024-03-28 00:13:24 +0100

    Delete intro work.sql

--- intro work.sql
+++
@@ -1,8 +0,0 @@
-SELECT TOP (1000) [EmployerID]
-      ,[FirstName]
-      ,[LastName]
-      ,[Age]
-      ,[Gernder]
-  FROM [SQL Project 1].[dbo].[EmployeeDemographics]
-
-  create view employeeDemographics
